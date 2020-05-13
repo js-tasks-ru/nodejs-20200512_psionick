cd8094e943a8fb59012d61c055fca3940f1fc102
Alex <psionickmobile@gmail.com>
2020-05-13 14:40:06 +0300

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,9 +1,9 @@
 function sum(a, b) {
-    if ((typeof a !== 'number') || (typeof b !== 'number')) {
-        throw new TypeError('Wrong argument type');
-    }
+  if ((typeof a !== 'number') || (typeof b !== 'number')) {
+    throw new TypeError('Wrong argument type');
+  }
 
-    return a + b;
+  return a + b;
 }
 
 module.exports = sum;
