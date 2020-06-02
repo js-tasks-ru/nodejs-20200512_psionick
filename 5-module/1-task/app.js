e9b8566dbbe28bcb397ba7f746edf17011557ddf
Alex <psionickmobile@gmail.com>
2020-06-02 23:06:19 +0300

    add solution for 5-module

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,11 +7,33 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
-
+let subscribers = {};
 router.get('/subscribe', async (ctx, next) => {
+  const id = Math.random();
+  const message = await new Promise((resolve) => {
+    subscribers[id] = resolve;
+    ctx.req.on('close', () => {
+      delete subscribers[id];
+    });
+  });
+  if (message) {
+    ctx.status = 200;
+    ctx.body = message;
+  }
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  ctx.status = 200;
+  if (message) {
+    for (const subscriber in subscribers) {
+      if ({}.hasOwnProperty.call(subscribers, subscriber)) {
+        subscribers[subscriber](message);
+      }
+    }
+
+    subscribers = Object.create(null);
+  }
 });
 
 app.use(router.routes());
