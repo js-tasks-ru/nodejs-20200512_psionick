3db599347fbfaf02899167297bb537931d3a7086
Alex <psionickmobile@gmail.com>
2020-06-03 10:10:24 +0300

    add solution for 6-module 1-task

--- 6-module/1-task/models/Category.js
+++ 6-module/1-task/models/Category.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
+const Schema = mongoose.Schema;
 
-const subCategorySchema = new mongoose.Schema({
+const subCategorySchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
 });
 
-const categorySchema = new mongoose.Schema({
+const categorySchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
   subcategories: [subCategorySchema],
 });
 
