b4904db6696e681036367f6c686fb2652dac8979
Alex <psionickmobile@gmail.com>
2020-07-15 20:41:52 +0300

    add solution for 8-module

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,8 +1,28 @@
+const Product = require('../models/Product');
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const product = await Product.findById(ctx.request.body.product);
+
+  const order = await Order.create({
+    user: ctx.user,
+    product: product,
+    phone: ctx.request.body.phone,
+    address: ctx.request.body.address,
+  });
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: {id: order._id, product},
+    to: ctx.user.email,
+    subject: 'Подтверждение заказа',
+  });
+
+  ctx.body = {order: order._id};
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orders = await Order.find({user: ctx.user});
+  ctx.body = {orders};
 };
