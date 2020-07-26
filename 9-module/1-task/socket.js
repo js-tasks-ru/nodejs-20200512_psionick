9cebff92ace21fc6df421e6a10e0c5262d3f8a90
Alex <psionickmobile@gmail.com>
2020-07-26 15:41:17 +0300

    add solution for 9-module

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,41 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const token = socket.handshake.query.token;
+
+    if (!token) {
+      return next(new Error('anonymous sessions are not allowed'));
+    }
+
+    const session = await Session.findOne({token}).populate('user');
+
+    if (!session) {
+      return next(new Error('anonymous sessions are not allowed'));
+    }
+
+    socket.session = session;
+
     next();
   });
 
-  io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+  io.on('connection', (socket) => {
+    socket.on('message', async (msg) => {
+      const date = new Date();
+      if (!msg) {
+        return;
+      }
+
+      try {
+        await Message.create({
+          user: socket.session.user.displayName,
+          chat: socket.session.user._id,
+          text: msg,
+          date,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
   });
 
   return io;
